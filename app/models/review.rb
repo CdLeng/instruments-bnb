69d5f121a802cafd264dff3a45d615c3e6ae6a84
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-02-17 22:46:33 +0100

    resolved carolinas comments. added a great than and less than for the reviews rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :instrument
 
   validates :rate, presence: true
-  validates :rate, numericality: { only_integer: true }
+  validates :rate, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :content, length: { maximum: 400 }
 end
