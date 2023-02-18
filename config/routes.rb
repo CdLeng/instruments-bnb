9450d195737b2c9a125995f4cf41f41083ea7ac8
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-02-18 11:50:29 +0100

    created a basic show action. Nothing viewable yet, as no instruments in our database

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "instruments", to: "instruments#index"
+  get "instruments/:id", to: "instruments#show"
 end
