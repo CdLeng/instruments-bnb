7af8896e0fc1ddcb158d1cc0e8f9c55b756e5c5c
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-02-23 23:13:55 +0100

    created a branch to add the review controller and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_instrument, only: %i[new create]
+
+  def new
+    @instrument = Instrument.find(params[:instrument_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.instrument = @instrument
+    @review.save
+    redirect_to instrument_path(set_instrument)
+  end
+
+  private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:instrument_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rate)
+  end
+
 end
