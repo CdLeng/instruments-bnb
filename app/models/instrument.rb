d63df88fa942d4f10eb9d7936b79e9ac83e36a7d
Coen de Leng <coendeleng@outlook.com>
2023-02-24 20:08:13 +0100

    Changed validation for price field of instrument model

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,6 +2,6 @@ class Instrument < ApplicationRecord
   belongs_to :instrument_category
   belongs_to :user
   validates :instrument_type, :price, :name, :address, presence: true
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: true
   validates :description, length: { maximum: 400 }
 end
