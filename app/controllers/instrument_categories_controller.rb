a8a5087ed3c67aca915d9d796749f863357e427d
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-02-25 19:28:18 +0000

    added the policy pundit to categories

--- app/controllers/instrument_categories_controller.rb
+++ app/controllers/instrument_categories_controller.rb
@@ -1,12 +1,15 @@
 class InstrumentCategoriesController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def index
     @categories = InstrumentCategory.all
-
+    @categories = policy_scope(InstrumentCategory)
   end
 
   def show
     @category = InstrumentCategory.find(params[:id])
     @all_instruments_category = Instrument.where(instrument_category: @category)
+    authorize @all_instruments_category
+    authorize @category
   end
 end
