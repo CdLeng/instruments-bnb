42891534a199f0304cef8157bb9c3e0670f2d9a4
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-02-26 10:03:37 +0000

    added dependent destroy to bookings

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,7 +1,7 @@
 class Instrument < ApplicationRecord
   belongs_to :instrument_category
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :price, :name, :address, presence: true
   validates :price, numericality: true
   validates :description, length: { maximum: 400 }
